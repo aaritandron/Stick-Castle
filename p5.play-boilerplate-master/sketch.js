d9a4e4a58985da65e3dc8b902fa8c55677d0c24b
aaritandron <64255372+aaritandron@users.noreply.github.com>
2020-07-18 17:11:40 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(1200,400);
+  createSprite(400, 350, 50, 500);
+  createSprite(450, 400, 50, 500);
+  createSprite(500, 450, 50, 500);
+  createSprite(550, 500, 50, 500);
+  createSprite(550, 500, 50, 500);
+  createSprite(550, 400, 50, 500);
+  createSprite(600, 350, 50, 500);
+
+  createSprite(750, 350, 50, 500);
+  createSprite(800, 400, 50, 500);
+  createSprite(850, 450, 50, 500);
+  createSprite(900, 400, 50, 500);
+  createSprite(950,350, 50, 500);
+
+  
+
+  
+
+}
+
+function draw() {
+  background(255,255,255); 
+   
+  drawSprites();
+}
